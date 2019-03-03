43b470cad46bd7ee96fa2f6574094b8489206644
DESKTOP-BGIH1OE\Leonardo <leonardo.bianchini90@gmail.com>
2019-03-03 16:37:42 +0100

    Yes e No after close dialog box

--- src/app/training/current-training/stop-training-component.ts
+++ src/app/training/current-training/stop-training-component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material';
+// Contiene roba interna di angular, tipo id del dialog boz o roba simile
 
 @Component({
     selector: 'app-stop-training',
     template: `<h1 mat-dialog-title>Are you sure?</h1>
+    <mat-dialog-content>
+        <p>You already got {{passedData.progress}}%
+    </mat-dialog-content>
     <mat-dialog-actions>
     <button mat-button [mat-dialog-close]="true">Yes</button>
     <button mat-button [mat-dialog-close]="false">No</button>
     </mat-dialog-actions>`
 })
 
-export class StopTrainingComponent { }
+export class StopTrainingComponent {
+    constructor(@Inject(MAT_DIALOG_DATA) private passedData: any) { }
+}
