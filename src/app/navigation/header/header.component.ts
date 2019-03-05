3679829cc1395c55f1f5633108aa18b1e09966f8
DESKTOP-KF4SFLC\Leonardo <leonardo.bianchini90@gmail.com>
2019-03-05 22:49:56 +0100

    Lez. 59 e 60. Aggiunta autenticazione (senza chiamata lato server), così da nascondere le pagine. Importante, i component si sottoscrivono ad un observer che li informa di quando l0utente si è loggato.

--- src/app/navigation/header/header.component.ts
+++ src/app/navigation/header/header.component.ts
@@ -1,20 +1,32 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
-
-  @Output() sidenavToggle = new EventEmitter<void>();
-
-  constructor() { }
-
-  ngOnInit() {
+export class HeaderComponent implements OnInit, OnDestroy {
+  ngOnDestroy() {
+    this.authSubscription.unsubscribe();
   }
 
-  onToggleSidenav(){
+  @Output() sidenavToggle = new EventEmitter<void>();
+  isAuth = false;
+  authSubscription: Subscription;
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+    // Si sottoscrive all'observer in ascolto su next() quando viene sparato
+    this.authSubscription = this.authService.authChange.subscribe(authStatus => {
+      this.isAuth = authStatus;
+    });
+  }
+
+  onToggleSidenav() {
     this.sidenavToggle.emit();
   }
 
