3405a925374d06e49c97ff8b6f1042675175843b
DESKTOP-KF4SFLC\Leonardo <leonardo.bianchini90@gmail.com>
2019-03-13 08:56:38 +0100

    [ntwrk][87] Aggiunto pipe sulla chiamata al db

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -4,6 +4,7 @@ import { Exercise } from '../exercise.model';
 import { NgForm } from '@angular/forms';
 import { AngularFirestore } from 'angularfire2/firestore';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-new-training',
@@ -11,16 +12,32 @@ import { Observable } from 'rxjs';
   styleUrls: ['./new-training.component.css']
 })
 export class NewTrainingComponent implements OnInit {
-  exercises: Observable<any>;
+  exercises: Observable<Exercise[]>;
   // Le classi iniettate tramite DI le inserisco qui nel costruttore
   // NB che per poterle usare, il motore di DI deve anche avere specificato il tipo di classe
   constructor(private trainingService: TrainingService, private db: AngularFirestore) { }
 
   ngOnInit() {
     //this.exercises = this.trainingService.getAvailableExercises();
-    this.exercises = this.db
+
+    // pipe() si usa perché stiamo usando rxjs > 6
+    this .exercises = this.db
       .collection('availableExercises')
-      .valueChanges();
+      .snapshotChanges()
+      .pipe(map(docArray => {
+        return docArray.map(doc => {
+          return {
+            id: doc.payload.doc.id,
+            // name: doc.payload.doc.data().name,
+            // duration: doc.payload.doc.data().duration,
+            // calories: doc.payload.doc.data().calories
+            ...doc.payload.doc.data()
+          } as Exercise;
+        });
+      }));
+      // .subscribe(result => {
+      //   console.log(result);
+      // })
   }
 
   onStartTraining(form: NgForm) {
