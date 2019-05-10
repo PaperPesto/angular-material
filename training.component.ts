cf4612fc3fdf79ebcf2dc549e0be5b503d8e09f2
Leonardo Bianchini <leonardo.bianchini90@gmail.com>
2019-05-10 07:52:05 +0200

    Mi era rimasto in canna da 2 mesi questo commit, non so a che serve e spero non rompa nulla

---
+++ training.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { TrainingService } from './training.service';
+
+@Component({
+  selector: 'app-training',
+  templateUrl: './training.component.html',
+  styleUrls: ['./training.component.css']
+})
+export class TrainingComponent implements OnInit {
+  onGoingTraining = false;
+  exerciseSubscription: Subscription;
+
+  constructor(private trainingService: TrainingService) { }
+
+  ngOnInit() {
+    this.exerciseSubscription = this.trainingService.exerciseChange.subscribe(
+      exercise => {
+        console.log('exercise', exercise);
+        if (exercise) {
+          console.log('true');
+          this.onGoingTraining = true;
+        } else {
+          console.log('true');
+          this.onGoingTraining = false;
+        }
+      }
+    );
+  }
+
+}
